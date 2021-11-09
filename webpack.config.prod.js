40a33798f90baa827312ec5f6ecf6514f11eef46
Han <allen0346@gmail.com>
2021-11-09 13:47:28 +0800

    Feature: build + publish flows are done

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,31 +1,18 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: {
-    app: [
-      './src/index.jsx',
-    ],
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    port: 3001,
+    index: './src/App.jsx',
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin(),
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    libraryTarget: 'commonjs2',
   },
   module: {
     rules: [
@@ -48,5 +35,10 @@ module.exports = {
   },
   resolve: {
     extensions: ['*', '.js', '.jsx'],
+  },
+  externals: {
+    react: 'react',
+    'react-dom': 'react-dom',
+    'styled-components': 'styled-components',
   },
 };
